6094d62d7a035f2e0e130034fba409c282ad1559
Salman Faris <salmanfaris.kambayang@gmail.com>
2024-12-14 18:56:47 +0800

    change to devUrl

--- lib/const/app_strings.dart
+++ lib/const/app_strings.dart
@@ -1,7 +1,6 @@
 class AppStrings {
   // Link KiosK Asmat
   static const String initialUrl = 'https://stg.kiosk.asmat.app/preview/BBB';
-  static const String localUrl = 'http://127.0.0.1:3333/preview/BBB';
   static const String testUrl = 'https://yonechka.github.io';
   static const String devUrl =
       'https://ce36-103-47-133-100.ngrok-free.app/preview/BBB';
